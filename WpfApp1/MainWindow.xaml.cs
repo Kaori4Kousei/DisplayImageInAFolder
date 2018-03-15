e4404150ee8f17a3642fa52852cb3719c13fb1ec
Namit <namit2saxena@gmail.com>
2018-03-15 13:00:06 +0530

    Added Image.

--- WpfApp1/MainWindow.xaml.cs
+++ WpfApp1/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -23,6 +24,13 @@ namespace WpfApp1
         public MainWindow()
         {
             InitializeComponent();
+            Loaded += PageLoaded;
+        }
+        private void PageLoaded(object sender, RoutedEventArgs e)
+        {
+            string[] fileEntries = Directory.GetFiles(@"E:\Cars\");
+            foreach (string fileName in fileEntries)
+                listView.Items.Add(fileName);
         }
     }
 }
