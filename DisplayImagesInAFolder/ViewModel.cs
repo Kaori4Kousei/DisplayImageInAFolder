fec99256a8152d200bc50b264324ecd3e705502d
Namit <namit2saxena@gmail.com>
2018-03-15 20:01:24 +0530

    Images

---
+++ DisplayImagesInAFolder/ViewModel.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace WpfApp1
+{
+    class ViewModel
+    {
+        private readonly ObservableCollection<ImagesPath> _imagePath;
+
+        public ObservableCollection<ImagesPath> ImagePath
+        {
+            get { return _imagePath; }
+        }
+
+        public ViewModel()
+        {
+            _imagePath = new ObservableCollection<ImagesPath>();
+            LoadingPath();
+        }
+
+        private void LoadingPath()
+        {
+            string[] fileEntries = Directory.GetFiles(@"E:\Cars\");
+            foreach (string fileName in fileEntries)
+                ImagePath.Add(new ImagesPath() { ImagePathString = fileName });
+        }
+    }
+}
